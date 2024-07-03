3a1bcec5e2d33f2488b5f8f989b871813486dbdf
WAHU0508 <sharongikenye@gmail.com>
2024-07-03 21:11:56 +0300

    reduce method

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 const totalBatteries = batteryBatches.reduce((accumulator, battery) => {
     return accumulator += battery;
 }, 0);
-console.log(totalBatteriesnpm)
